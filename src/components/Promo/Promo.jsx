dcf7dedf314af58a5cff57702cb33495da25988b
unknown <m.a.krishtopa@yandex.ru>
2023-08-04 01:33:43 +0300

    feat: make header, navigation, promo adaptive for 768 and 320px

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -4,25 +4,22 @@ import promoLogo from "../../images/promo-logo.svg";
 export default function Promo() {
     return (
         <section className='promo'>
-            <div className='promo__content'>
-                <div className='promo__description'>
-                    <h1 className='promo__title'>
-                        Учебный проект студента факультета
-                        <br /> Веб-разработки.
-                    </h1>
-                    <p className='promo__subtitle'>
-                        Листайте ниже, чтобы узнать больше про этот проект и его
-                        создателя.
-                    </p>
-                    <button className='promo__button'>Узнать больше</button>
-                </div>
-                <img
-                    src={promoLogo}
-                    alt='Глобус'
-                    className='promo__logo
-                '
-                />
+            <div className='promo__description'>
+                <h1 className='promo__title'>
+                    Учебный проект студента факультета Веб&#8209;разработки.
+                </h1>
+                <p className='promo__subtitle'>
+                    Листайте ниже, чтобы узнать больше про этот проект и его
+                    создателя.
+                </p>
+                <button className='promo__button'>Узнать больше</button>
             </div>
+            <img
+                src={promoLogo}
+                alt='Глобус'
+                className='promo__logo
+                '
+            />
         </section>
     );
 }
