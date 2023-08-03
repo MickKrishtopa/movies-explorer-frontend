6ac481a9f6c744481229859a7626eb7c0164fba3
unknown <m.a.krishtopa@yandex.ru>
2023-08-03 11:35:18 +0300

    feat: make component aboutroject

---
+++ src/components/AboutProject/AboutProject.jsx
@@ -0,0 +1,39 @@
+import "./AboutProject.css";
+
+export default function AboutProject() {
+    return (
+        <section className='about-project'>
+            <h2 className='about-project__title'>О проекте</h2>
+            <div className='about-project__text-area'>
+                <div className='about-profect__content-left'>
+                    <p className='about-project__subtitle'>
+                        Дипломный проект включал 5 этапов
+                    </p>
+                    <p className='about-project__description'>
+                        Составление плана, работу над бэкендом, вёрстку,
+                        добавление функциональности и финальные доработки.
+                    </p>
+                </div>
+                <div className='about-profect__content-right'>
+                    <p className='about-project__subtitle'>
+                        На выполнение диплома ушло 5 недель
+                    </p>
+                    <p className='about-project__description'>
+                        У каждого этапа был мягкий и жёсткий дедлайн, которые
+                        нужно было соблюдать, чтобы успешно защититься.
+                    </p>
+                </div>
+            </div>
+            <div className='about-project__decoration-area'>
+                <p className='about-project__decoration-text about-project__decoration-text_type_green'>
+                    1 неделя
+                </p>
+                <p className='about-project__decoration-text about-project__decoration-text_type_gray'>
+                    4 недели
+                </p>
+                <p className='about-project__decoration-text'>Back-end</p>
+                <p className='about-project__decoration-text'>Front-end</p>
+            </div>
+        </section>
+    );
+}
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import "./Main.css";
 import Promo from "../Promo/Promo";
+import AboutProjest from "../AboutProject/AboutProject";
 
 export default function Main() {
     return (
         <>
             <Promo />
+            <AboutProjest />
         </>
     );
 }
